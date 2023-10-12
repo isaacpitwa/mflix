f868c5ebd26a8fd54d9002e7d99081759c0f25bb
Isaac Pitwa <isaacpitwa256@gmail.com>
2023-10-12 13:37:21 +0300

    Include Single movies API

--- pages/api/movies/[_id].ts
+++ pages/api/movies/[_id].ts
@@ -1,9 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import clientPromise from "../../../lib/mongodb";
+import { ObjectId } from "mongodb";
 
-export default  (req: NextApiRequest, res: NextApiResponse)=>{
+export default  async (req: NextApiRequest, res: NextApiResponse)=>{
     try {
         const { _id } = req.query;
-        
+        if(!_id){
+            return res.status(400).json({message : "Missing movie id"});
+        }
+        const client  =  await clientPromise;
+        const db = client.db("mflix");
+        const movie = await db.collection("movies").findOne({_id : new ObjectId(_id.toString())});
+        res.json(movie);
     } catch (error) {
         console.log(error);
     }
